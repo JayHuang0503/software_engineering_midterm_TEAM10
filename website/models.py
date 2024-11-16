fccfe60e863a9527a431368790c6c501ca7c2ceb
JordenHuang <jorden90573@gmail.com>
2024-11-16 15:05:28 +0800

    feat: 新增開課名額與剩餘名額至課程資料表

--- website/models.py
+++ website/models.py
@@ -27,24 +27,28 @@ class Students(db.Model, UserMixin):
 class Courses(db.Model):
     '''
     Attributes:
-        course_id    : 課程代碼, string
-        course_name  : 課程名稱, string
-        teacher_name : 教師姓名, string
-        credit       : 學分數, integer
-        course_type  : 課程類別 (必修/選修), string
-        weekday      : 星期, string
-        course_time  : 節次, string
-        lang         : 授課語言, string
-        course_for   : 開課班級, string
+        course_id       : 課程代碼, string
+        course_name     : 課程名稱, string
+        teacher_name    : 教師姓名, string
+        credit          : 學分數, integer
+        course_type     : 課程類別 (必修/選修), string
+        course_quota    : 開課名額
+        remaining_quota : 剩餘名額
+        weekday         : 星期, string
+        course_time     : 節次, string
+        lang            : 授課語言, string
+        course_for      : 開課班級, string
     '''
     __tablename__ = "courses"
 
-    def __init__(self, course_id, course_name, teacher_name, credit, course_type, weekday, course_time, lang, course_for) -> None:
+    def __init__(self, course_id, course_name, teacher_name, credit, course_type, course_quota, remaining_quota, weekday, course_time, lang, course_for) -> None:
         self.course_id = course_id
         self.course_name = course_name
         self.teacher_name = teacher_name
         self.credit = credit
         self.course_type = course_type
+        self.course_quota = course_quota
+        self.remaining_quota = remaining_quota
         self.weekday = weekday
         self.course_time = course_time
         self.lang = lang
@@ -58,13 +62,15 @@ class Courses(db.Model):
     teacher_name = db.Column(db.String(50))
     credit = db.Column(db.Integer)
     course_type = db.Column(db.String(2))
+    course_quota = db.Column(db.Integer)
+    remaining_quota = db.Column(db.Integer)
     weekday = db.Column(db.String(50))
     course_time = db.Column(db.String(50))
     lang = db.Column(db.String(50))
     course_for = db.Column(db.String(50))
 
     def __repr__(self):
-        return f"course id: {self.course_id}, course name: {self.course_name}, teacher name: {self.teacher_name}, credit: {self.credit}, course type: {self.course_type}, weekday: {self.weekday}, course time: {self.course_time}, language: {self.lang}, course for: {self.course_for}"
+        return f"course id: {self.course_id}, course name: {self.course_name}, teacher name: {self.teacher_name}, credit: {self.credit}, course type: {self.course_type}, course quota: {self.course_quota}, remaining quota: {self.remaining_quota}, weekday: {self.weekday}, course time: {self.course_time}, language: {self.lang}, course for: {self.course_for}"
 
     def to_dict(self):
         return {field.name:getattr(self, field.name) for field in self.__table__.c}
